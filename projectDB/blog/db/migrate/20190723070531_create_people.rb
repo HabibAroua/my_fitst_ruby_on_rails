00b6e80badaa85a8b709665485f5854756aa12f9
Habib Aroua <HabibAroua@users.noreply.github.com>
2019-07-23 08:17:24 +0100

    Update 20190723070531_create_people.rb

--- projectDB/blog/db/migrate/20190723070531_create_people.rb
+++ projectDB/blog/db/migrate/20190723070531_create_people.rb
@@ -1,8 +1,26 @@
 class CreatePeople < ActiveRecord::Migration[5.1]
-  def change
-    create_table :people do |t|
+  
+  #def change
+    #create_table :people do |t|
 
+      #t.timestamps
+    #end
+  #end
+  
+  def up
+    create_table :persons do |t|
+      t.column "firstName" , :string , :limit => 60
+      t.column "lastName" , :string , :limit =>60
+      t.string "email" , :default=> '' , :null=>false
+      t.string "password" , :limit =>255
+      #t.datetime "create_at"
+                           #==>t.timestamps
+      #t.datetime "update_at"
       t.timestamps
     end
+  end
+  
+  def down
+    drop_table :persons
   end
 end
