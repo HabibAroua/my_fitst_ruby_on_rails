aee09d22b9ce3b061aaf572a39be53c1d241a14b
Habib Aroua <HabibAroua@users.noreply.github.com>
2019-07-23 09:45:08 +0100

    migration succed

--- projectDB/blog/db/schema.rb
+++ projectDB/blog/db/schema.rb
@@ -10,6 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20190723070531) do
+
+  create_table "persons", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
+    t.string "firstName", limit: 60
+    t.string "lastName", limit: 60
+    t.string "email", default: "", null: false
+    t.string "password"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
