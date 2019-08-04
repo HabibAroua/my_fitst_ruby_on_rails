3e2dbba389acd5bab666e67ae0bfdff7849f3366
Habib Aroua <HabibAroua@users.noreply.github.com>
2019-08-04 14:09:41 +0100

    before the migration

--- projectDB/blog/db/migrate/20190804115834_create_topics.rb
+++ projectDB/blog/db/migrate/20190804115834_create_topics.rb
@@ -1,8 +1,15 @@
 class CreateTopics < ActiveRecord::Migration[5.1]
-  def change
+  
+  def up
     create_table :topics do |t|
-
+      t.string "name"
+      t.integer "position"
+      t.boolean "visible", :default=>false
       t.timestamps
     end
+  end
+  
+  def down
+    drop_table :topics
   end
 end
--- projectDB/blog/db/migrate/20190804120143_create_pages.rb
+++ projectDB/blog/db/migrate/20190804120143_create_pages.rb
@@ -1,8 +1,19 @@
 class CreatePages < ActiveRecord::Migration[5.1]
-  def change
+  
+  def up
     create_table :pages do |t|
-
+      t.integer "topic_id"
+      t.string "name"
+      t.string "permalink"
+      t.integer "postition"
+      t.boolean "visible" , default=> false
       t.timestamps
     end
+    add_index("pages","topic_id")
+    add_index("pages","permalink")
+  end
+  
+  def down
+    drop_table :pages
   end
 end
