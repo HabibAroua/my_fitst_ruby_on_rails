01b3f2aeb9cc1f588c1421750505a12bda0ecc14
Habib Aroua <HabibAroua@users.noreply.github.com>
2019-08-09 10:54:28 +0100

    Update topic.rb

--- projectDB/blog/app/models/topic.rb
+++ projectDB/blog/app/models/topic.rb
@@ -1,7 +1,7 @@
 class Topic < ApplicationRecord
 	
 	scope :search , lambda {|query| where (["name LIKE ? ","%#{query}#"])}
-	scope :newest_first , lambda{order (created_at DESC)}
+	scope :newest_first , lambda{order ("created_at DESC")}
 	scope :visible , lambda{ where(:visible => true) }
 	scope :invisible , lambda{where(:visible => false ) }
 	scope :sorted , lambda {order(:position)}
