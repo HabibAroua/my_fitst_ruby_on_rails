2db5050b3a8cc2521a47a76cba3a48c335f44431
Habib Aroua <HabibAroua@users.noreply.github.com>
2019-08-09 09:45:04 +0100

    add new lambda

--- projectDB/blog/app/models/topic.rb
+++ projectDB/blog/app/models/topic.rb
@@ -1,2 +1,9 @@
 class Topic < ApplicationRecord
+	
+	scope :search , lambda {|query| where (["name LIKE ? ","%#{query}#"])}
+	scope :newest_first , lambda{order (created_at DESC)}
+	scope :visible, lambda{ where(:visible => true) }
+	scope :invisible, lambda{where(:visible => false ) }
+	scope :sorted , lambda {order(:position)}
+	
 end
