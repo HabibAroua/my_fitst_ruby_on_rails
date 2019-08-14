87aa0b17934f36b4692b53dc4478cf81f8f59651
Habib Aroua <HabibAroua@users.noreply.github.com>
2019-08-14 10:22:11 +0100

    Update 20190814090953_admin_user_page_creation.rb

--- projectDB/blog/db/migrate/20190814090953_admin_user_page_creation.rb
+++ projectDB/blog/db/migrate/20190814090953_admin_user_page_creation.rb
@@ -1,4 +1,13 @@
 class AdminUserPageCreation < ActiveRecord::Migration[5.1]
-  def change
+  def up
+    create_table :admin_users_pages do |t|
+      t.integer "id_admin_users"
+      t.integer "page_id"
+    end
+    add_index("id_admin_users","page_id")
+  end
+  
+  def down
+    drop_table :admin_users_pages
   end
 end
