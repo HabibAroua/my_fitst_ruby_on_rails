de0bab9351fdb034bd52e0c7d49c66a470aa4b9c
Habib Aroua <HabibAroua@users.noreply.github.com>
2019-09-01 12:47:46 +0100

    Update schema.rb

--- store/db/schema.rb
+++ store/db/schema.rb
@@ -10,6 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20190901110218) do
+
+  create_table "products", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
+    t.string "name"
+    t.string "description"
+    t.integer "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
