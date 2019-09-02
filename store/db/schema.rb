121b6340bd014caba32475042791865f6b1e6a7b
Habib Aroua <HabibAroua@users.noreply.github.com>
2019-09-02 16:35:27 +0100

    add new table

--- store/db/schema.rb
+++ store/db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190901110218) do
+ActiveRecord::Schema.define(version: 20190902145228) do
+
+  create_table "pins", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
+    t.string "title"
+    t.text "caption"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "products", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
     t.string "name"
