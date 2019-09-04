14331a4d97d3c1f360094f19a6d22793f2e41cfe
Habib Aroua <HabibAroua@users.noreply.github.com>
2019-09-04 13:06:27 +0100

    delete some project

--- store/config/routes.rb
+++
@@ -1,8 +0,0 @@
-Rails.application.routes.draw do
-  root to: 'pins#index'
-  resources :pins
-  resources :products
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  #get 'products/index'
-  #get 'products/new'
-end
