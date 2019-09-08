e31d34630ca702b98b9943404589fd06e2868a7c
Habib Aroua <HabibAroua@users.noreply.github.com>
2019-09-09 00:24:19 +0100

    Update application_controller.rb

--- authentification/app/controllers/application_controller.rb
+++ authentification/app/controllers/application_controller.rb
@@ -2,4 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
   skip_before_action :authenticate_user!, only: :principal #name of view
+  
+  #file:///C:/RailsInstaller/Ruby2.3.3/lib/ruby/gems/2.3.0/gems/devise-4.7.1/app/views/devise/registrations/new.html.erb
+  #you should see this link for authenticate
 end
