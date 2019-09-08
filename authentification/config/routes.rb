1391293eabedf66b079425f5316200996c2e7f4e
Habib Aroua <HabibAroua@users.noreply.github.com>
2019-09-08 18:34:53 +0100

    change the name of file index to principal to redirect to login page

--- authentification/config/routes.rb
+++ authentification/config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   resources :posts
   devise_for :users
   get 'habib/login'
-  get 'habib/index'
+  get 'habib/principal'
   get 'habib/welcome'
   
-  root to: "habib#index"
+  root to: "habib#principal"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
