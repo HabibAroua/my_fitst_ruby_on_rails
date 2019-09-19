7a8c32cf76e7f1515365e4939362ab11be56f320
Habib Aroua <HabibAroua@users.noreply.github.com>
2019-09-19 11:52:42 +0100

    add new controllers

--- authentification/config/routes.rb
+++ authentification/config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
   resources :posts
-  devise_for :users
+  #devise_for :users
+  devise_for :users, controllers: { registrations: 'users/registrations', sessions: 'users/sessions' }
+  get 'users/sign_in'
   get 'habib/login'
   get 'habib/principal'
   get 'habib/welcome'
-  
   root to: "habib#principal"
+  devise_for :users, skip: [:sessions]
+  
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
